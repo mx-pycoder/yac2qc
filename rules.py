3e6977db1bac7078c50956315df13dbbc201a680
mx <mx@pycoder.nl>
2015-03-22 14:06:10 +0100

    Fixed typo and added LICENCE to rules.py

--- rules.py
+++ rules.py
@@ -2,6 +2,28 @@
 
 ''' category mapping rules for yac2qc '''
 
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 mx[replace-this]pycoder.nl
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from collections import namedtuple as _namedtuple
 
 # a mapping rule consists of the following fields:
@@ -32,7 +54,7 @@ rules = [
     _rule('car - fuel', 'BA', 'special fuels incorporated', None, None),
     # similar for 'CleanFuelInc'
     _rule('car - fuel', 'BA', 'CleanFuelInc', None, None),
-    # with my ST means 'deposit'
+    # with my bank ST means 'deposit'
     _rule('cash - deposit', 'ST', None, None, None),
     # .. and 'GM' means 'withdrawal' at a cash machine
     _rule('cash - withdrawal', 'GM', None, None, None),
@@ -51,3 +73,4 @@ rules = [
     # 'description' field, this is a statement related to income tax
     _rule('taxes - income tax', None, None, '999999999999', '1111.1111.111'),
 ]
+
